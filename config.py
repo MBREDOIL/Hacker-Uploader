b3cc5f60b656623b400812bc8ff6d47aa9ad8135
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-05-08 12:24:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-api_id =  22923037
-api_hash  = 'dfb3666878b3851460a58461c5a50f5b'
-bot_token =  '7458363295:AAFQ3tc4q_xVN-xI-2XkHamaF-vYeqmyR9Y'
-OWNER =  6554343173
-OWNER_USERNAME = 'vighnesh9820'
-LOG =  -1002540145139
-ADMINS = [OWNER, 6554343173]
+api_id =  25579552
+api_hash  = 'ac24e438ff9a0f600cf3283e6d60b1aa'
+bot_token = os.environ.get("bot_token", "")
+OWNER =  6556141430
+OWNER_USERNAME = 'tor3k7'
+LOG =  -1002303860733
+ADMINS = [OWNER, 6556141430]
